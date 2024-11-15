767809277a4c209d5a7f663fedd9df2c96f55571
bazil-1854 <bscs22072@itu.edu.pk>
2024-11-15 22:16:16 +0500

    Initialize MongoDb connection ...

--- server.js
+++ server.js
@@ -1,16 +1,14 @@
 const express = require('express');
-const app = express();
-const cors = require('cors'); 
-const PORT = 3001;
+const dotenv = require('dotenv');
+const cors = require('cors');
+const connectDB = require('./mongoDb/db');  
+ 
+dotenv.config();
+connectDB();
 
+const app = express();
 app.use(cors());
 app.use(express.json());
  
-// Data 
-app.get('/api/listings', (req, res) => {
-  res.json("Final Phase");
-});
-
-app.listen(PORT, () => {
-  console.log(`Server is on http://localhost:${PORT}`);
-});
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
